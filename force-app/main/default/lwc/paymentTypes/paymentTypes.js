2d2f55c317bf1c38e757d3f09a4ce81f606f74c1
Mykola Kanunnikov <kanunnikovmykola@gmail.com>
2023-03-29 18:06:38 +0300

    LWC base

--- force-app/main/default/lwc/paymentTypes/paymentTypes.js
+++ force-app/main/default/lwc/paymentTypes/paymentTypes.js
@@ -1,5 +1,19 @@
 import { LightningElement } from 'lwc';
 
+const columns = [
+    { label: 'Active', fieldName: 'active', type: 'boolean'},
+    { label: 'Payment Type Label', fieldName: 'label', type: 'text' },
+    { label: 'Payment Type', fieldName: 'type', type: 'text' },
+];
+
 export default class PaymentTypes extends LightningElement {
-    
+
+    data = [];
+    columns = columns;
+    rowOffset = 0;
+
+
+    handleClick(){
+        this.rowOffset += 1;
+    }
 }
