f0c7ccc1b7ffac62f2ad05a4af3ad2a94b27ca6f
Mykola Kanunnikov <kanunnikovmykola@gmail.com>
2023-03-29 20:03:52 +0300

    mtd record retrivement

--- force-app/main/default/lwc/paymentTypes/paymentTypes.js
+++ force-app/main/default/lwc/paymentTypes/paymentTypes.js
@@ -1,19 +1,49 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import getRecords from '@salesforce/apex/LocationController.getRecords';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 const columns = [
-    { label: 'Active', fieldName: 'active', type: 'boolean'},
+    { label: 'Active', fieldName: 'active', type: 'boolean' },
     { label: 'Payment Type Label', fieldName: 'label', type: 'text' },
     { label: 'Payment Type', fieldName: 'type', type: 'text' },
 ];
 
 export default class PaymentTypes extends LightningElement {
 
+    @api
+    recordId;
+
     data = [];
     columns = columns;
     rowOffset = 0;
 
+    connectedCallback() {
+        getRecords({ recordId: this.recordId })
+            .then(resp => {
+                if (resp.isSuccess) {
+                    this.data = resp.responseObj;
+                } else {
+                    const errorEvent = new ShowToastEvent({
+                        title: 'Error',
+                        variant: 'error',
+                        message: resp.responseObj
+                    });
+                    this.dispatchEvent(errorEvent);
+                }
+            })
+            .catch(error => {
+                const errorEvent = new ShowToastEvent({
+                    title: 'Error',
+                    variant: 'error',
+                    message: "Error: " + error.body.message
+                });
+                this.dispatchEvent(errorEvent);
+            })
 
-    handleClick(){
+    }
+
+
+    handleClick() {
         this.rowOffset += 1;
     }
 }
